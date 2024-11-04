754fee9a1f50a516e17735b63e1fe61745399964
Diogo Pinto <109294288+diogopinto1@users.noreply.github.com>
2024-11-04 01:10:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Sabia que sim babe, até sermos velhinhos <3";
+  question.innerHTML = "Sabia que sim babe, AMO TE";
   gif.src =
     "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmp1dGE0amRlMmh3aG53dmJtOGV2Ymp1ZjNlZG1kMGtvdmw2dHNxZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/x28cIQSn19Tbi/giphy.gif";
 });
